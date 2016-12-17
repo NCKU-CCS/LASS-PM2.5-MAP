74df9a5a0303ad8a72a4110b230f671b61d6b069
bhchen <bohengorz@gmail.com>
2016-12-17 23:09:53 +0800

    1.add route and trend of history 2.change idw code

--- leaflet-idw.js
+++ leaflet-idw.js
@@ -20,23 +20,24 @@
         this._max = 1;
         this._data = [];
     }
-    
+
     simpleidw.prototype = {
 
         defaultCellSize: 25,
 
         defaultGradient: {
-            0.0: '#000066',
-            0.1: 'blue',
-            0.2: 'cyan',
-            0.3: 'lime',
-            0.4: 'yellow',            
-            0.5: 'orange',
-            0.6: 'red',
-            0.7: 'Maroon',
-            0.8: '#660066',
-            0.9: '#990099',
-            1.0: '#ff66ff'
+
+		0.00: "#ffffff",
+                0.11: "#CAE6AA",
+                0.23: "#29FD2E",
+                0.35: "#1FCA23",
+                0.41: "#FFFD38",
+                0.47: "#FCC553",
+                0.53: "#E26C22",
+                0.58: "#D89695",
+                0.64: "#FC0D1B",
+                0.70: "#7E0308",
+                1: "#6F359E"
         },
 
         data: function (data) {
@@ -104,7 +105,7 @@
         draw: function (opacity) {
             if (!this._cell) this.cellSize(this.defaultCellSize);
             if (!this._grad) this.gradient(this.defaultGradient);
-            
+
             var ctx = this._ctx;
 
             ctx.clearRect(0, 0, this._width, this._height);
@@ -118,16 +119,17 @@
 
             // colorize the heatmap, using opacity value of each pixel to get the right color from our gradient
             var colored = ctx.getImageData(0, 0, this._width, this._height);
-            this._colorize(colored.data, this._grad, opacity);
-            
+
+	    this._colorize(colored.data, this._grad, opacity);
             ctx.putImageData(colored, 0, 0);
+
 
             return this;
         },
 
         _colorize: function (pixels, gradient, opacity) {
             for (var i = 0, len = pixels.length, j; i < len; i += 4) {
-                j = pixels[i + 3] * 4; 
+                j = pixels[i + 3] * 4;
 
                     pixels[i] = gradient[j];
                     pixels[i + 1] = gradient[j + 1];
@@ -270,65 +272,121 @@ L.IdwLayer = (L.Layer ? L.Layer : L.Class).extend({
             exp = this.options.exp === undefined ? 1 : this.options.exp,
             max = this.options.max === undefined ? 1 : this.options.max,
             maxZoom = this.options.maxZoom === undefined ? this._map.getMaxZoom() : this.options.maxZoom,
-            v = 1, 
+            v = 1,
             cellCen = r / 2,
             grid = [],
             nCellX = Math.ceil((bounds.max.x-bounds.min.x)/r)+1,
             nCellY = Math.ceil((bounds.max.y-bounds.min.y)/r)+1,
             panePos = this._map._getMapPanePos(),
 
-            offsetX = 0, 
+            offsetX = 0,
             offsetY = 0,
             i, len, p, cell, x, y, j, len2, k;
-            
-            console.log(nCellX);
-            console.log(nCellY);
-            
-            console.time('process');
-        
-        for (i = 0, len = nCellY; i < len; i++) {
-            for (j = 0, len2 = nCellX; j < len2; j++) {     
-            
-                var x=i*r,y=j*r;
-                var numerator=0,denominator=0;
-                
-                for (k = 0, len3 = this._latlngs.length; k < len3; k++) {          
-                
-                    var p = this._map.latLngToContainerPoint(this._latlngs[k]);                    
-                    var cp = L.point((y-cellCen), (x-cellCen));                    
-                    var dist = cp.distanceTo(p);
-                    
-                    var val =
-                            this._latlngs[k].alt !== undefined ? this._latlngs[k].alt :
-                            this._latlngs[k][2] !== undefined ? +this._latlngs[k][2] : 1;
-                    
-                    if(dist===0){
-                            numerator = val;
-                            denominator = 1;
-                            break;
-                    }
-                    
-                    var dist2 = Math.pow(dist, exp);
 
-                    numerator += (val/dist2);
-                    denominator += (1/dist2);             
-                            
-                }
-                
-                interpolVal = numerator/denominator;
-                
-                cell = [j*r, i*r, interpolVal];
-                
-                if (cell) {
-                    data.push([
-                        Math.round(cell[0]),
-                        Math.round(cell[1]),
-                        Math.min(cell[2], max)
-                    ]);
-                }
-            }
-        }
-        console.timeEnd('process');
+            console.time('process ' + this._latlngs.length);
+
+        // -- cclljj
+        var rt = map.containerPointToLatLng([0, 0]);
+        var lb = map.containerPointToLatLng([map.getSize().x, map.getSize().y]);
+	var offset_x = Math.abs(rt.lng - lb.lng) * 111.32 * Math.cos(Math.PI * rt.lat / 180) ;// map.getSize().x;  // km per map.size unit
+	var offset_x = Math.abs(rt.lng - lb.lng) * 111.32  / map.getSize().x;  // km per map.size unit
+
+	var offset_y = Math.abs(rt.lat - lb.lat) * 110.574 / map.getSize().y;  // km per map.size unit
+	var station_range = 10; //km
+
+	var cellsn = new Array(nCellY);
+	var cellsd = new Array(nCellY);
+		for (var i = 0; i < nCellY; i++) {
+			cellsn[i] = new Array(nCellX);
+			cellsd[i] = new Array(nCellX);
+			for (var j = 0; j < nCellX; j++){
+				cellsn[i][j] = 0;
+				cellsd[i][j] = 0;
+			}
+		}
+
+        for (k = 0, len3 = this._latlngs.length; k < len3; k++) {
+        	var p = this._map.latLngToContainerPoint(this._latlngs[k]);
+		var x1 = p.x - station_range / offset_x;
+		var x2 = p.x + station_range / offset_x;
+		var y1 = p.y - station_range / offset_y;
+		var y2 = p.y + station_range / offset_y;
+		x1 = Math.round(x1 / r + 0.5);
+		x2 = Math.ceil(x2 / r - 0.5);
+		if (x1 < 0) { x1 = 0; }
+		if (x2 < 0) { continue; }
+		if (x1 >= nCellX) { continue; }
+		if (x2 >= nCellX) { x2 = nCellX - 1; }
+		y1 = Math.round(y1 / r + 0.5) ;
+		y2 = Math.ceil(y2 / r - 0.5) ;
+		if (y1 < 0) { y1 = 0; }
+		if (y2 < 0) { continue; }
+		if (y1 >= nCellY) { continue; }
+		if (y2 >= nCellY) { y2 = nCellY - 1; }
+
+		for (var j=x1; j<= x2; j++){
+			for (var i = y1; i<= y2; i++){
+				if (cellsd[i][j] < 0) {
+					continue;
+				}
+
+                    		var p = this._map.latLngToContainerPoint(this._latlngs[k]);
+                    		var cp = L.point((j*r-cellCen), (i*r-cellCen));
+ 	                   	var dist = cp.distanceTo(p);
+				var LJ_x = (p.x - cp.x) * offset_x;
+				var LJ_y = (p.y - cp.y) * offset_y;
+				var LJ_dist = Math.sqrt(LJ_x * LJ_x + LJ_y * LJ_y);
+				if (LJ_dist > station_range) {
+					continue;
+				}
+
+				var dist2;
+
+				var val =
+                            		this._latlngs[k].alt !== undefined ? this._latlngs[k].alt :
+                            		this._latlngs[k][2] !== undefined ? +this._latlngs[k][2] : 1;
+
+                    		if(dist==0){
+                            		cellsn[i][j] = val;
+                            		cellsd[i][j] = -1;
+                    		} else {
+
+                    			dist2 = Math.pow(dist, exp);
+ 		                   	cellsn[i][j] += (val/dist2);
+                		    	cellsd[i][j] += (1/dist2);
+				}
+
+			}
+		}
+
+
+	}
+
+        for (i = 0, len = nCellY; i < len; i++) {
+            	for (j = 0, len2 = nCellX; j < len2; j++) {
+			if (cellsd[i][j] <0) { cellsd[i][j] = 1; }
+
+			if (cellsd[i][j] == 0) {
+				interpolVal = 0;
+			} else {
+				interpolVal = cellsn[i][j]/cellsd[i][j];
+			}
+
+                	cell = [j*r, i*r, interpolVal];
+
+                	if (cell && interpolVal) {
+                    		data.push([
+                        		Math.round(cell[0]),
+                        		Math.round(cell[1]),
+                        		Math.min(cell[2], max)
+                    		]);
+			}
+
+		}
+	}
+
+
+        console.timeEnd('process ' + this._latlngs.length);
         console.time('draw ' + data.length);
         this._idw.data(data).draw(this.options.opacity);
         console.timeEnd('draw ' + data.length);
